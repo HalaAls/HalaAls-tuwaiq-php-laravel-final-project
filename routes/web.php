782b5e3853def0f23b7d682dfc67b314933821e7
Halaals <hala.alsuwait@gmail.com>
2024-04-07 05:56:29 +0300

    update web

--- routes/web.php
+++ routes/web.php
@@ -2,11 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\Dashboard;
-
+use App\Http\Controllers\Shopping;
+use Illuminate\Support\Facades\Auth;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/logout', [Dashboard::class, 'Logout'])->name('logout');
 
 
 Route::get('/dashboard', [Dashboard::class, 'Index'])->name('index');
@@ -17,6 +20,18 @@ Route::post('/dashboard/updateproduct', [Dashboard::class, 'UpdateProduct'])->na
 Route::post('/dashboard/searchproducts', [Dashboard::class, 'SearchProducts'])->name('searchproducts');
 Route::get('/dashboard/showproducts', [Dashboard::class, 'ShowProducts'])->name('showproducts');
 
+Route::get('/dashboard/productdetails', [Dashboard::class, 'GetProductDetails'])->name('productDetails');
+Route::post('/dashboard/createproductdetail', [Dashboard::class, 'CreateProductDetails'])->name('createproductdetails');
+Route::get('/dashboard/deleteproductdetail/{id}', [Dashboard::class, 'DeleteProductDetail'])->name('deleteproductdetail');
+Route::post('/dashboard/updateproductdetail', [Dashboard::class, 'UpdateProductDetail'])->name('updateproductdetail');
+Route::post('/dashboard/searchproductdetail', [Dashboard::class, 'SearchProductDetail'])->name('searchproductdetail');
+Route::get('/dashboard/showproductdetail', [Dashboard::class, 'ShowProductDetail'])->name('showproductdetail');
+
+
+
+Route::get('/shopping/listItem', [Shopping::class, 'ShowListItem'])->name('listItem');
+Route::get('/shopping/details/{id}', [Shopping::class, 'ShowDetail'])->name('details');
+Route::get('/shopping/addtocart/{id}', [Shopping::class, 'AddToCart'])->name('addtocart');
 
 
 
